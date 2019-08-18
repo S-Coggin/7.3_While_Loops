0b690664b14ca8e4f9ab604202fde94d21ab98bd
S-Coggin <53793753+S-Coggin@users.noreply.github.com>
2019-08-18 15:59:46 -0400

    Add files via upload
    *commented out script*

--- index.js
+++ index.js
@@ -1,34 +1,34 @@
-alert("CONNECTED");
-var count = -10;
+// alert("CONNECTED");
+// var count = -10;
 
-while(count <= 10) {
-    console.log(count);
-    count++;
-}
-//print numbers between -10 and 19
+// while(count <= 10) {
+//     console.log(count);
+//     count++;
+// }
+// //print numbers between -10 and 19
 
-var count = 10;
+// var count = 10;
 
-while(count <=40) {
-    console.log(count);
-    count+=2;
-}
-//print all even numbers between 300 and 333 in console
+// while(count <=40) {
+//     console.log(count);
+//     count+=2;
+// }
+// //print all even numbers between 300 and 333 in console
 
 
-var count = 300;
+// var count = 300;
 
-while(count <=330) {
-    console.log(count);
-    count+=3;
-} //print all odd numbers between 300 and 333 in console
+// while(count <=330) {
+//     console.log(count);
+//     count+=3;
+// } //print all odd numbers between 300 and 333 in console
 
-var count = 5;
+// var count = 5;
 
-while(count <=50) {
-if(count % 3 === 0 && count % 5 === 0){
-    console.log(count); 
-}
-count++;
-}
-//print all numbers divisible by 3 AND 5 between 5 and 50
+// while(count <=50) {
+// if(count % 3 === 0 && count % 5 === 0){
+//     console.log(count); 
+// }
+// count++;
+// }
+// //print all numbers divisible by 3 AND 5 between 5 and 50
